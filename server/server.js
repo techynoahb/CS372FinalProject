ff6b4a169e280e1141e113f860e33543c97ce2ba
CloudEerie <irodriguez10@alaska.edu>
2026-03-28 15:39:15 -0800

    Makeshift attempt at adding Mongoose to server.js, feel free to revert if it causes issues on your end

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config() // Make a server.env file w/ MONGO_URI=[connection string]/Project
 
 // to spin up a server run "node server.js" in the terminal.
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB error: ', err))
 
 app.get('/', (req, res) => {
   res.send('Server is running!')
