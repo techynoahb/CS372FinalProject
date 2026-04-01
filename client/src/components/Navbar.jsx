63a014e29ff57251b7bef16907b689cd9c4a3576
Noah Bush <noah@Noahs-MacBook-Pro-2.local>
2026-04-01 07:03:41 -0800

    fixed text alignment in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
@@ -10,9 +11,10 @@ function Navbar() {
   return (
     <AppBar position="fixed"> {/* makes navbar stick to the top of the page */}
       <Toolbar variant = "dense"> {/* mui library*/}
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" sx={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
           Cinema Scenes: Movie Host
         </Typography>
+        <Box sx={{ flexGrow: 1 }} />
         <Button color="inherit" component={Link} to="/home">Home</Button>
         {!user && <Button color="inherit" component={Link} to="/login">Login</Button>}
         {user && <Button color="inherit" component={Link} to="/gallery">Gallery</Button>}
